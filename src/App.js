a9543660ec9ab858e08b00af9bbc6df1a525afbe
Charles-Antoine Dupuy <charlesantoine.dupuy@gmail.com>
2020-11-06 16:02:45 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "./App.css";
+import Search from "./components/Search";
+import Task from "./components/Task";
+
+function App() {
+  return (
+    <>
+      <Search />
+      <Task />
+    </>
+  );
+}
+
+export default App;
